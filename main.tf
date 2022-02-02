d0baacec7c7da0455922b917ef89177b60811437
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-02-02 16:26:29 +0100

    security grp edited

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "web-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
