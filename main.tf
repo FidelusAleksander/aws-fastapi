76027d47e2dc59c1ccbf4fd707423ef48272b650
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-02-02 15:21:10 +0100

    MOdify tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "aws-fastapi"
+
+    workspaces {
+      name = "aws-fastapi"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "eu-west-1"
 }
