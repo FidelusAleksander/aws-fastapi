fca46c9f93bda1dabe1cbe17dd31520f8de94879
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-02-02 17:28:46 +0100

    add https ingress

--- main.tf
+++ main.tf
@@ -49,6 +49,14 @@ resource "aws_security_group" "web-sg" {
   }
 
   ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
