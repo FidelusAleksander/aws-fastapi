2c3bc69e74103411309dc4cdb60260638ecde6d9
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-02-02 16:07:33 +0100

    CHange ami

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ provider "aws" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-00ae935ce6c2aa534"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
