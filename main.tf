3fdb3860085666dd46213c9b58f06aebb82aed7a
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-02-02 16:57:16 +0100

    update user data

--- main.tf
+++ main.tf
@@ -29,8 +29,11 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 80 &
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1> Hello, world </h1>" > /var/www/html/index.html
               EOF
 }
 
