3811ff7f219634bf93a8b174f259b581101cd5e6
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-02-02 16:46:16 +0100

    change port

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p 80 &
               EOF
 }
 
