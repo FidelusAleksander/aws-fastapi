823a3a4546889fecc0d2027143ec749c9ed72677
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-02-02 17:13:27 +0100

    Edit sg

--- main.tf
+++ main.tf
@@ -39,10 +39,29 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "web-sg" {
   name = "fastapi-web-sg"
+
   ingress {
-    from_port   = 80
-    to_port     = 80
+    name             = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    name        = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    name        = "All traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
