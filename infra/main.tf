51fd5adc735454be91eb4dd6efb7803cd89b16d5
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-03-30 21:22:06 +0200

    Dynamic block on ingress, cancel automatic deploys

--- infra/main.tf
+++ infra/main.tf
@@ -40,27 +40,14 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "fastapi-web-sg"
 
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = [22, 80, 443]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
