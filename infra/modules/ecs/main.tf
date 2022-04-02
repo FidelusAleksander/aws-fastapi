916ee35ba6c670b1d36d9077da0ea2e5b21e54c1
FidelusAleksander <alek.fidelus@gmail.com>
2022-04-02 16:16:44 +0200

    ECS service added

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -19,3 +19,46 @@ resource "aws_ecs_task_definition" "task_definition" {
 locals {
   ecr_image = "${var.ecr_repository_url}:${var.image_tag}"
 }
+
+data "aws_vpc" "main" {
+  id = "vpc-54c9792d"
+}
+data "aws_subnet_ids" "subnet_ids" {
+  vpc_id = data.aws_vpc.main.id
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "${var.project_name}-ecs-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = data.aws_subnet_ids.subnet_ids.ids
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_task_sg.id]
+  }
+}
+
+
+resource "aws_security_group" "ecs_task_sg" {
+  name   = "${var.project_name}-ecs-task-sg"
+  vpc_id = data.aws_vpc.main.id
+
+  dynamic "ingress" {
+    for_each = [443, 80]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
