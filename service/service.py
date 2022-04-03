b892263625ff129d43d168475e7d4438453b2309
FidelusAleksander <alek.fidelus@gmail.com>
2022-04-03 21:12:55 +0200

    Service wip

---
+++ service/service.py
@@ -0,0 +1,13 @@
+import boto3
+
+
+class Service:
+    def __init__(self, s3_client):
+        self._s3_client = s3_client
+
+    @classmethod
+    def from_environ(cls):
+        return cls(s3_client=boto3.Session().client('s3'))
+
+    def list_objects(self):
+        return self._s3_client.list_objects_v2(Bucket="aws-fastapi-bucket")
