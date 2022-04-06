6a7eb9fae458fbb6eded61ea06478993cde6fa39
FidelusAleksander <alek.fidelus@gmail.com>
2022-04-06 22:25:03 +0200

    add key to test function

--- tests/test_service.py
+++ tests/test_service.py
@@ -5,4 +5,4 @@ def test_list_objects(service, test_data):
 
 
 def test_generate_presigned_url(service, test_data):
-    service.generate_presigned_url()
+    service.generate_presigned_url(key=test_data[0])
