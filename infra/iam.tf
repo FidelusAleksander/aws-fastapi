c766c762eab257810a677524e6b1cbb1daf272db
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-04-12 11:40:30 +0200

    Restructure TF files

---
+++ infra/iam.tf
@@ -0,0 +1,21 @@
+module "iam_ecs" {
+  source              = "./modules/iam/ecs"
+  project_name        = var.project_name
+}
+
+module "iam_s3_for_ecs" {
+  source         = "./modules/iam/s3"
+  role_id        = module.iam_ecs.ecs_tasks_container_role_id
+  s3_bucket_name = var.s3_bucket_name
+}
+
+module "iam_ecr_for_ecs" {
+  source              = "./modules/iam/ecr"
+  role_id             = module.iam_ecs.ecs_execution_role_id
+  ecr_repository_name = var.ecr_repository_name
+}
+
+module "iam_logs_for_ecs" {
+  source              = "./modules/iam/logs"
+  role_id             = module.iam_ecs.ecs_tasks_container_role_id
+}
